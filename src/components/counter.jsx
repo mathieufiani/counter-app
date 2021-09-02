fdad1449cb28fd82d583aff59245bd2764ad1ef9
Mathieu Fiani <mathieu.fiani2@gmail.com>
2021-09-02 14:29:28 +0200

    added delete btn

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,30 +2,35 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
   styles = {
     fontSize: 10,
     fontWeight: "bold",
   };
 
-  handleIncrements = (product) => {
+  handleIncrements = () => {
     this.setState({ value: this.state.value + 1 });
   };
   render() {
     return (
-      <React.Fragment>
-        {this.props.children}
+      <div>
         <span style={this.styles} className={this.changeColorCountBtn()}>
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrements({ id: 1 })}
+          onClick={this.handleIncrements}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-      </React.Fragment>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
     );
   }
   changeColorCountBtn() {
