808da92f447aeb8706424873454d8fe8e9b425c7
Mathieu Fiani <mathieu.fiani2@gmail.com>
2021-09-02 14:34:03 +0200

    added reset btn

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -16,9 +16,14 @@ class Counters extends Component {
     this.setState({ counters });
   };
   render() {
-    console.log("props", this.props);
     return (
       <React.Fragment>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
