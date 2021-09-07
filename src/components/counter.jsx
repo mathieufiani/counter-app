6277f7284f8c736fd4d1e63fc892e3ad087c9abf
Mathieu Fiani <mathieu.fiani2@gmail.com>
2021-09-07 11:44:35 +0200

    added decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,12 +17,12 @@ class Counter extends Component {
     console.log("counter - Rendered");
     return (
       <div className="container row">
-        <div className="col col-sm p-0">
+        <div className="col-1 m-2">
           <span style={this.styles} className={this.changeColorCountBtn()}>
             {this.formatCount()}
           </span>
         </div>
-        <div className="col col-sm p-0">
+        <div className="col">
           <button
             onClick={() => this.props.onIncrement(this.props.counter)}
             className="btn btn-secondary btn-sm"
@@ -32,14 +32,16 @@ class Counter extends Component {
           <button
             className="btn btn-secondary btn-sm m-2"
             onClick={() => this.props.onDecrement(this.props.counter)}
+            disabled={this.disableBtn()}
           >
             -
           </button>
           <button
             onClick={() => this.props.onDelete(this.props.counter.id)}
-            className="btn btn-danger btn-sm m-2"
+            className="btn btn-danger btn-sm"
+            style={this.styles}
           >
-            Delete
+            x
           </button>
         </div>
       </div>
@@ -54,6 +56,10 @@ class Counter extends Component {
   formatCount() {
     const { value } = this.props.counter;
     return value === 0 ? "zero" : value;
+  }
+  disableBtn() {
+    const { value } = this.props.counter;
+    return value === 0;
   }
 }
 
